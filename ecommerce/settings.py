f86a1b7aac49531f07f0d46661fba5a117a4a0db
jatin <jatin.kshatriya2821@gmail.com>
2022-06-27 16:40:10 +0530

    debug false

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,5 +139,5 @@ STATICFILES_DIRS=[(os.path.join(BASE_DIR,'ecommerce/static'))]
 
 
 # Media Folder Settings
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
